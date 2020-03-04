581b4df395e55cf58c3a340df02a5c9c9c97c120
Pharia Le <pharia.le@gmail.com>
2020-03-04 23:07:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
   new_arr = []
   
   array.each_with_index do |ele, i|
-    if array.length -1 == i
+    if (array.length-1) == i
       new_arr << "and #{ele}"
-    elsif array.length -2 == i
+    elsif (array.length-2) == i
       new_arr << "#{ele} "
     else
       new_arr << "#{ele} ,"
