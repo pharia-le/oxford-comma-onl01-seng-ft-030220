d6fdcce5bfa9ec4400acf025f8fb10f6873313af
Pharia Le <pharia.le@gmail.com>
2020-03-04 23:18:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2 
+    return array.join(" and ")
+  end
+  
+  new_arr = []
+  
+  array.each_with_index do |ele, i|
+    if (array.length-1) == i
+      new_arr << "and #{ele}"
+    else
+      new_arr << "#{ele}, "
+    end
+  end
+  
+  new_arr.join
 end
